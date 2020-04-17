3ca3e0106320336c4061b27ce957151e50339320
hanjie <95hanjie@gmail.com>
2020-04-17 17:03:36 +0800

    update: status bar

--- library/src/main/java/com/imhanjie/support/statusbar/StatusBarUtil.java
+++ library/src/main/java/com/imhanjie/support/statusbar/StatusBarUtil.java
@@ -16,9 +16,9 @@ import android.view.View;
 public class StatusBarUtil {
 
     /**
-     * 暗色状态栏
+     * 亮色状态栏
      */
-    public static void setDarkMode(Activity activity) {
+    public static void setLightMode(Activity activity) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
@@ -28,13 +28,13 @@ public class StatusBarUtil {
     /**
      * 布局延伸至状态栏
      */
-    public static void setLayoutFullScreen(Activity activity, boolean darkMode) {
+    public static void setLayoutFullScreen(Activity activity, boolean lightMode) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
             activity.getWindow().getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (darkMode) {
+                if (lightMode) {
                     activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
                     StatusBarUtil.compatThirdRomStatusBar(activity, true);
                 }
@@ -45,9 +45,9 @@ public class StatusBarUtil {
     /**
      * 兼容国内第三方定制 ROM 白色状态栏
      */
-    private static void compatThirdRomStatusBar(Activity activity, boolean isDark) {
-        FlymeStatusbarColorUtil.setStatusBarDarkIcon(activity, isDark);
-        MIUIStatusBarUtil.setStatusBarDarkMode(isDark, activity);
+    private static void compatThirdRomStatusBar(Activity activity, boolean isLight) {
+        FlymeStatusbarColorUtil.setStatusBarDarkIcon(activity, isLight);
+        MIUIStatusBarUtil.setStatusBarDarkMode(isLight, activity);
     }
 
     /**
@@ -55,9 +55,6 @@ public class StatusBarUtil {
      */
     public static int getStatusBarHeight(Activity activity) {
         // 获取状态栏高度
-        if (activity == null) {
-            return 0;
-        }
         int resourceId = activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
         return activity.getResources().getDimensionPixelSize(resourceId);
     }
