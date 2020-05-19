7b8bf5e4e5a0d16697d99facc8d0817d5264a8ab
hanjie <95hanjie@gmail.com>
2020-05-19 15:18:53 +0800

    feat: dpi spi

--- library/src/main/java/com/imhanjie/support/ext/Primitive.kt
+++ library/src/main/java/com/imhanjie/support/ext/Primitive.kt
@@ -4,36 +4,66 @@ import android.util.TypedValue
 import androidx.core.content.ContextCompat
 import com.imhanjie.support.AndroidUtils
 
-val Float.dp
+val Float.dp: Float
     get() = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_DIP,
         this,
         AndroidUtils.APP.resources.displayMetrics
     )
 
-val Float.sp
+val Float.dpi: Int
+    get() = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        this,
+        AndroidUtils.APP.resources.displayMetrics
+    ).toInt()
+
+val Float.sp: Float
     get() = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_SP,
         this,
         AndroidUtils.APP.resources.displayMetrics
     )
 
-val Int.dp
+val Float.spi: Int
+    get() = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_SP,
+        this,
+        AndroidUtils.APP.resources.displayMetrics
+    ).toInt()
+
+val Int.dp: Float
     get() = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_DIP,
         this.toFloat(),
         AndroidUtils.APP.resources.displayMetrics
     )
 
-val Int.sp
+val Int.dpi: Int
+    get() = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        this.toFloat(),
+        AndroidUtils.APP.resources.displayMetrics
+    ).toInt()
+
+val Int.sp: Float
     get() = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_SP,
         this.toFloat(),
         AndroidUtils.APP.resources.displayMetrics
     )
+
+val Int.spi: Int
+    get() = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_SP,
+        this.toFloat(),
+        AndroidUtils.APP.resources.displayMetrics
+    ).toInt()
 
 val Int.string
     get() = AndroidUtils.APP.getString(this)
+
+fun Int.string(vararg formatArgs: Any?) = AndroidUtils.APP.getString(this, formatArgs)
 
 val Int.drawable
     get() = ContextCompat.getDrawable(AndroidUtils.APP, this)
